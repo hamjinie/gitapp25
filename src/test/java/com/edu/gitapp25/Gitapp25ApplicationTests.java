bb312b386545053de1955100f6ad4bc7ca846575
jini <gkawlsgml77@gmail.com>
2023-04-24 19:55:47 +0900

    first commit

---
+++ src/test/java/com/edu/gitapp25/Gitapp25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gitapp25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitapp25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
